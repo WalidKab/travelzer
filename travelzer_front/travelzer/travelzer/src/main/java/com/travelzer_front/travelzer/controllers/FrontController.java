18444e0a07c06175c334225a59af2c7403822908
Walid <walid.kabli@le-campus-numerique.fr>
2021-10-29 13:53:58 +0200

    Reservation + facturation

--- travelzer_front/travelzer/travelzer/src/main/java/com/travelzer_front/travelzer/controllers/FrontController.java
+++ travelzer_front/travelzer/travelzer/src/main/java/com/travelzer_front/travelzer/controllers/FrontController.java
@@ -2,6 +2,7 @@ package com.travelzer_front.travelzer.controllers;
 
 import com.travelzer_front.travelzer.models.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class FrontController {
@@ -42,15 +45,15 @@ public class FrontController {
     @GetMapping(value = "/vehicule/{id}")
     public String showVehicle(Model model, @PathVariable int id){
         Vehicule vehicule = new RestTemplate().getForObject(urlCatalogue + "/vehicule/" + id, Vehicule.class);
-        Reservation[] reservations = new RestTemplate().getForObject(urlReservation + "/reservations/" , Reservation[].class);
-        List<Reservation> filteredReservation = new ArrayList<>();
-        for (Reservation reservation : reservations){
-            if(reservation.getVehicule_id() == id){
-                filteredReservation.add(reservation);
-            }
-        }
+//        Reservation[] reservations = new RestTemplate().getForObject(urlReservation + "/reservations/" , Reservation[].class);
+//        List<Reservation> filteredReservation = new ArrayList<>();
+//        for (Reservation reservation : reservations){
+//            if(reservation.getVehicule_id() == id){
+//                filteredReservation.add(reservation);
+//            }
+//        }
         model.addAttribute("vehicule", vehicule);
-        model.addAttribute("reservations", filteredReservation);
+//        model.addAttribute("reservations", filteredReservation);
         return "product";
     }
 
@@ -66,7 +69,7 @@ public class FrontController {
     }
 
     @PostMapping(value = "/vehicule/{id}/reserve")
-    public String addReservation(@ModelAttribute("reservation") Reservation reservationForm, @ModelAttribute("vehicule") Vehicule vehicule) {
+    public String addReservation(@ModelAttribute("reservation") Reservation reservationForm, @ModelAttribute("vehicule") Vehicule vehicule, RedirectAttributes redirectAttributes) {
 
         boolean userExist = new RestTemplate().getForObject(urlUsers + "/licencenumber/" + reservationForm.getLicenceNumber(), boolean.class);
 
@@ -75,7 +78,6 @@ public class FrontController {
         }
         else{
             User user = new RestTemplate().getForObject(urlUsers + "/getuser/" + reservationForm.getLicenceNumber(), User.class);
-            String a = "test";
 
             Reservation reservation = new Reservation();
             reservation.setDatestart(reservationForm.getDatestart());
@@ -85,9 +87,11 @@ public class FrontController {
             reservation.setEstimatedkm(reservationForm.getEstimatedkm());
             reservation.setUser_id(user.getId());
 
+            redirectAttributes.addFlashAttribute("reservation",reservation);
+
             new RestTemplate().postForObject(urlReservation + "/reservation", reservation, Reservation.class);
 
-            return "redirect:/catalogue";
+            return "redirect:/recap";
         }
     }
 
@@ -104,9 +108,6 @@ public class FrontController {
     public String addUser(@ModelAttribute("user") User userForm){
         User user = new User();
 
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = userForm.getBirthday();
-
         user.setFirstname(userForm.getFirstname());
         user.setLastname(userForm.getLastname());
         user.setBirthday(userForm.getBirthday());
@@ -119,5 +120,25 @@ public class FrontController {
         new RestTemplate().postForObject(urlUsers + "/user", user, User.class);
 
         return "redirect:/catalogue";
+    }
+
+    @GetMapping(value = "/recap")
+    public String recapReservation(Model model,@ModelAttribute("reservation") Reservation reservationForm){
+
+        Vehicule vehicule = new RestTemplate().getForObject(urlCatalogue + "/vehicule/" + reservationForm.getVehicule_id(), Vehicule.class);
+
+        float price;
+        if (Objects.equals(vehicule.getCategory().getName(), "Moto")){
+            price = (float) (vehicule.getCategory().getBaseprice() + vehicule.getModel().getCc() * 0.001 * vehicule.getModel().getPriceperkm() * reservationForm.getEstimatedkm());
+        } else if (Objects.equals(vehicule.getCategory().getName(), "Utilitaire")){
+            price = (float) (vehicule.getCategory().getBaseprice() + vehicule.getModel().getVolume() * 0.05 * vehicule.getModel().getPriceperkm() * reservationForm.getEstimatedkm());
+        } else {
+            price = vehicule.getCategory().getBaseprice() + vehicule.getModel().getPriceperkm() * reservationForm.getEstimatedkm();
+        }
+
+        model.addAttribute("price", price);
+
+        model.addAttribute("vehicule", vehicule);
+        return "recap";
     }
 }
